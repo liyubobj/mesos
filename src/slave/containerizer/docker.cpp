cc05e42fc43efcfe9581db779c899c7705d91589
Yu Bo Li <liyubobj@cn.ibm.com>
2016-10-11 06:31:39 +0000

    Removed TODO message for docker killing.
    Review: https://reviews.apache.org/r/52735

--- src/slave/containerizer/docker.cpp
+++ src/slave/containerizer/docker.cpp
@@ -2190,12 +2190,6 @@ void DockerContainerizerProcess::__destroy(
   Container* container = containers_.at(containerId);
 
   if (!kill.isReady() && !container->status.future().isReady()) {
-    // TODO(benh): This means we've failed to do a Docker::kill, which
-    // means it's possible that the container is still going to be
-    // running after we return! We either need to have a periodic
-    // "garbage collector", or we need to retry the Docker::kill
-    // indefinitely until it has been sucessful.
-
     string failure = "Failed to kill the Docker container: " +
                      (kill.isFailed() ? kill.failure() : "discarded future");
 
